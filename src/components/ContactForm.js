a0139e7e3f76bca2bb9f06fde229fefbbff842ba
Alexis Winters <alexiscodes@yahoo.com>
2023-04-01 07:48:15 -0400

    validateContactForm

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,12 +1,13 @@
 import { Button, Label, Col, FormGroup } from "reactstrap";
-import { Formik, Field, Form } from "formik";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import { validateContactForm } from "../utils/validateContactForm";
 
 const ContactForm = () => {
-    const handleSubmit = (values, { resetForm }) => {
-        console.log('form values:', values);
-        console.log('in JSON format:', JSON.stringify(values));
-        resetForm();
-    };
+  const handleSubmit = (values, { resetForm }) => {
+    console.log("form values:", values);
+    console.log("in JSON format:", JSON.stringify(values));
+    resetForm();
+  };
 
   return (
     <Formik
@@ -20,6 +21,7 @@ const ContactForm = () => {
         feedback: "",
       }}
       onSubmit={handleSubmit}
+      validate={validateContactForm}
     >
       <Form>
         <FormGroup row>
@@ -28,6 +30,7 @@ const ContactForm = () => {
           </Label>
           <Col md="10">
             <Field name="firstName" placeholder="First Name" className="form-control" />
+            <ErrorMessage name="firstName">{(msg) => <p className="text-danger">{msg}</p>}</ErrorMessage>
           </Col>
         </FormGroup>
         <FormGroup row>
@@ -36,6 +39,7 @@ const ContactForm = () => {
           </Label>
           <Col md="10">
             <Field name="lastName" placeholder="Last Name" className="form-control" />
+            <ErrorMessage name="lastName">{(msg) => <p className="text-danger">{msg}</p>}</ErrorMessage>
           </Col>
         </FormGroup>
         <FormGroup row>
@@ -44,6 +48,7 @@ const ContactForm = () => {
           </Label>
           <Col md="10">
             <Field name="phoneNum" placeholder="Phone" className="form-control" />
+            <ErrorMessage name="phoneNum">{(msg) => <p className="text-danger">{msg}</p>}</ErrorMessage>
           </Col>
         </FormGroup>
         <FormGroup row>
@@ -51,22 +56,18 @@ const ContactForm = () => {
             Email
           </Label>
           <Col md="10">
-            <Field name="email" placeholder="Email" type='email' className="form-control" />
+            <Field name="email" placeholder="Email" type="email" className="form-control" />
+            <ErrorMessage name="email">{(msg) => <p className="text-danger">{msg}</p>}</ErrorMessage>
           </Col>
         </FormGroup>
         <FormGroup row>
           <Label check md={{ size: 4, offset: 2 }}>
-            <Field
-                name='agree'
-                type='checkbox'
-                className='form-check-input'
-                />{' '}
-            May we contact you?
+            <Field name="agree" type="checkbox" className="form-check-input" /> May we contact you?
           </Label>
           <Col md="4">
-            <Field name='contactType' as='select' className="form-control">
-                <option>By Phone</option>
-                <option>By Email</option>
+            <Field name="contactType" as="select" className="form-control">
+              <option>By Phone</option>
+              <option>By Email</option>
             </Field>
           </Col>
         </FormGroup>
@@ -75,15 +76,15 @@ const ContactForm = () => {
             Your Feedback
           </Label>
           <Col md="10">
-            <Field name='feedback' as='textarea' rows='12' className="form-control" />
+            <Field name="feedback" as="textarea" rows="12" className="form-control" />
           </Col>
         </FormGroup>
         <FormGroup row>
-            <Col md={{ size: 10, offset: 2 }}>
-                <Button type='submit' color='primary'>
-                    Send Feedback
-                </Button>
-            </Col>
+          <Col md={{ size: 10, offset: 2 }}>
+            <Button type="submit" color="primary">
+              Send Feedback
+            </Button>
+          </Col>
         </FormGroup>
       </Form>
     </Formik>
